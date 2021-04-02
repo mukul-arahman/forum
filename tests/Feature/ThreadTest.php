3cdc691e4209b83c63e3bbb3cdc236d99b7ec3d3
mukul-arahman <mukul.arahman@gmail.com>
2021-04-02 21:50:44 +0600

    Episode-2

---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+}

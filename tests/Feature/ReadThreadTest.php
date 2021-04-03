e9beb3aac429882bebe337cbff6aa43e03cb55fe
mukul-arahman <mukul.arahman@gmail.com>
2021-04-03 07:47:10 +0600

    Episode 3

---
+++ tests/Feature/ReadThreadTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ReadThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertStatus(200)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_single_thread()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}

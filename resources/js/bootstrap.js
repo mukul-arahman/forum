3d0041231d36d2a8815674fd13a23661a7acaa52
mukul-arahman <mukul.arahman@gmail.com>
2021-04-28 22:32:06 +0600

    Episode 29, flash message with vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,19 +23,11 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+window.Vue = require('vue');
 
-// import Echo from 'laravel-echo';
+window.events = new Vue();
 
-// window.Pusher = require('pusher-js');
+window.flash = function (message) {
+    window.events.$emit('flash', message);
+};
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });

f53cee252ccfaf6b28ef34e855d3c9b4abdbda27
mukul-arahman <mukul.arahman@gmail.com>
2021-04-28 12:44:16 +0600

    Episode 27

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Activity;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -19,23 +20,7 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return mixed
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()
-                    ->latest()
-                    ->with('subject')
-                    ->take(50)
-                    ->get()
-                    ->groupBy( function ($activity) {
-                        return $activity->created_at->format('Y-m-d');
-                    });
     }
 }
--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -23,13 +23,11 @@ class ProfilesTest extends TestCase
     /** @test */
     public function profiles_display_all_threads_created_by_the_associated_user()
     {
-        $this->withoutExceptionHandling();
+        $this->singIn();
 
-        $user = create('App\User');
+        $thread = create('App\Thread', ['user_id' => auth()->id()]);
 
-        $thread = create('App\Thread', ['user_id' => $user->id]);
-
-        $this->get("/profiles/{$user->name}")
+        $this->get("/profiles/" . auth()->user()->name)
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
